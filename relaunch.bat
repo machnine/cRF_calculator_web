f8bdfb366518b654a4f151e311b497b80b5f0227
Mian Chen <mianchen@gmail.com>
2022-07-25 11:46:12 +0100

    Update relaunch.bat

--- relaunch.bat
+++ relaunch.bat
@@ -1,5 +1,5 @@
 @echo off
 FOR /f "tokens=*" %%i IN ('docker images --filter "dangling=true" --format "{{.ID}}"') DO docker rmi %%i
 docker build -t crfcalc:latest .
-docker run -d -p 4000:80 crfcalc
+docker run -d -p 4000:80 --restart always crfcalc
 @echo on
